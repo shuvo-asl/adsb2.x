fabbec16a0cb3cc48ceaba0c97da586f8ed1f5c3
MD Mehedi Hasan <shuvo@asl.aero>
2024-05-20 16:37:30 +0600

    feat(geo filtering): Implement filter() of GeoFilter

--- src/core/geofilter.py
+++ src/core/geofilter.py
@@ -1,12 +1,41 @@
 from core.abstracts.pipeline import StreamFilter
+import fiona
+from shapely.geometry import shape, Point
+import json
+import os
 
 class GeoFilter(StreamFilter):
 
-    def __init__(self,input_stream):
+    def __init__(self, input_stream):
         self.input_stream = input_stream
         super().__init__(input_stream)
-    
-    async def filter(self):
+        os.environ['SHAPE_RESTORE_SHX'] = 'YES'
 
-        async for item in self.input_stream:
-            yield item
+    async def filter(self):
+        # Correct the path to point to the shapefile location
+        shape_file_path = os.path.join(os.path.dirname(__file__), '../data/shapes/bg_airspace_polygon.shp')
+
+        if not os.path.exists(shape_file_path):
+            raise FileNotFoundError(f"Shapefile not found: {shape_file_path}")
+
+        try:
+            # Open the shapefile
+            with fiona.open(shape_file_path) as fiona_collection:
+                async for item in self.input_stream:
+
+                    item_decode = json.loads(item)
+                    lat = item_decode.get('lat')
+                    lon = item_decode.get('lon')
+
+                    if lat is not None and lon is not None:
+                        point = Point(lon, lat)  # longitude, latitude
+
+                        shapefile_record = next(iter(fiona_collection))
+
+                        # Convert the record's geometry to a Shapely shape
+                        geom_shape = shape(shapefile_record['geometry'])
+                        if geom_shape.contains(point):
+                            # If the point is within the feature's geometry, yield the item
+                            yield item
+        except fiona.errors.DriverError as e:
+            print(f"Fiona failed to open the shapefile: {e}")
--- src/index.py
+++ src/index.py
@@ -3,15 +3,13 @@ from helper.config_helper import getConfig
 from core.sensor_network import SensorNetwork
 from core.deduplicator import Deduplicator
 from core.geofilter import GeoFilter
-
+import json
 
 async def open_data(input_stream):
     async for item in input_stream:
         print(item)
 
-
 async def main():
-
     all_sensors = getConfig("sensors")
 
     sensorNetwork = SensorNetwork(all_sensors)
@@ -20,11 +18,9 @@ async def main():
     geo_filter = GeoFilter(deduplicator.filter())
 
     data_task = asyncio.create_task(open_data(geo_filter.filter()))
-
     sensor_network_task = asyncio.create_task(sensorNetwork.run())
 
     await asyncio.gather(data_task, sensor_network_task)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
